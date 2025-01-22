0d9000f96e571fa8d50448ea8961f78b64d38a50
Madhavan <madhavan@atatus.com>
2025-01-22 18:24:58 +0530

    fix : dubug print correct line number and file name wrong.

--- debugX/debugX.go
+++ debugX/debugX.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/fatih/color"
@@ -45,6 +46,12 @@ var (
 	filePrinter  = color.New(color.FgHiWhite).Add(color.BgHiRed).SprintFunc()
 )
 
+var (
+	callTree   = make(map[string][]string)
+	callTreeMu sync.Mutex
+	rootFunc   string
+)
+
 // Get the current timestamp
 func getTimestamp() string {
 	return time.Now().Format("15:04:05")
@@ -77,7 +84,6 @@ func DebugControl(levels ...string) {
 				resEnabled = true
 			case flow_checker:
 				flowCheckerEnabled = true
-			default:
 			case all:
 				// If 'ALL' is explicitly passed, disable all debug levels
 				allDebugEnabled = false
@@ -90,28 +96,41 @@ func DebugControl(levels ...string) {
 // Log an info message if enabled
 func Info(format string, args ...interface{}) {
 	if allDebugEnabled || infoEnabled {
-		logMessage(info, infoPrinter, format, args...)
+		formattedMessage := fmt.Sprintf(format, args...)
+		file, line := getCallerInfo()
+		time := getTimestamp()
+		fmt.Println(timePrinter(fmt.Sprintf("Time: %s", time)), filePrinter(fmt.Sprintf("File: %s", file)), linePrinter(fmt.Sprintf("Line:%d", line)), infoPrinter(fmt.Sprintf("%s: %s", "INFO", formattedMessage)))
+
 	}
 }
 
 // Log an error message if enabled
 func Error(format string, args ...interface{}) {
 	if allDebugEnabled || errorEnabled {
-		logMessage(err, errorPrinter, format, args...)
+		formattedMessage := fmt.Sprintf(format, args...)
+		file, line := getCallerInfo()
+		time := getTimestamp()
+		fmt.Println(timePrinter(fmt.Sprintf("Time: %s", time)), filePrinter(fmt.Sprintf("File: %s", file)), linePrinter(fmt.Sprintf("Line:%d", line)), errorPrinter(fmt.Sprintf("%s: %s", "ERROR", formattedMessage)))
 	}
 }
 
 // Log a warning message if enabled
 func Warn(format string, args ...interface{}) {
 	if allDebugEnabled || warnEnabled {
-		logMessage(warn, warnPrinter, format, args...)
+		formattedMessage := fmt.Sprintf(format, args...)
+		file, line := getCallerInfo()
+		time := getTimestamp()
+		fmt.Println(timePrinter(fmt.Sprintf("Time: %s", time)), filePrinter(fmt.Sprintf("File: %s", file)), linePrinter(fmt.Sprintf("Line:%d", line)), warnPrinter(fmt.Sprintf("%s: %s", "INFO", formattedMessage)))
 	}
 }
 
 // Log a result message if enabled
 func Res(format string, args ...interface{}) {
 	if allDebugEnabled || resEnabled {
-		logMessage(res, resPrinter, format, args...)
+		formattedMessage := fmt.Sprintf(format, args...)
+		file, line := getCallerInfo()
+		time := getTimestamp()
+		fmt.Println(timePrinter(fmt.Sprintf("Time: %s", time)), filePrinter(fmt.Sprintf("File: %s", file)), linePrinter(fmt.Sprintf("Line:%d", line)), resPrinter(fmt.Sprintf("%s: %s", "INFO", formattedMessage)))
 	}
 }
 
@@ -146,4 +165,89 @@ func FlowChecker(params ...interface{}) func() {
 	}
 
 	return func() {}
+}
+
+// Function to capture the current function's name
+func getCurrentFunctionName(skip int) string {
+	pc, _, _, _ := runtime.Caller(skip)
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return "unknown"
+	}
+	return fn.Name()
+}
+
+// Function to log function calls dynamically
+func logFunctionCall(parent, child string) {
+	callTreeMu.Lock()
+	defer callTreeMu.Unlock()
+	callTree[parent] = append(callTree[parent], child)
+}
+
+// Dynamic wrapper for root function
+func InvokeAndTrack(f func()) string {
+	// Set the root function
+	rootFunc = getCurrentFunctionName(2)
+
+	// Wrap and track function execution
+	callTree = make(map[string][]string)
+	wrappedFunc := func() {
+		trackFunctionCalls(func() {
+			defer func() { recover() }() // Recover from errors
+			f()
+		})
+	}
+	wrappedFunc()
+
+	// Generate and return the flow tree
+	return generateFlowTree(rootFunc)
+}
+
+// Function to track calls dynamically using runtime stack
+func trackFunctionCalls(f func()) {
+	pc, _, _, _ := runtime.Caller(1) // Get caller info
+	_ = runtime.FuncForPC(pc).Name()
+	callStack := []string{}
+
+	defer func() {
+		for i := 1; ; i++ {
+			pc, _, _, ok := runtime.Caller(i)
+			if !ok {
+				break
+			}
+			fn := runtime.FuncForPC(pc).Name()
+			callStack = append(callStack, fn)
+		}
+		for i := len(callStack) - 1; i > 0; i-- {
+			logFunctionCall(callStack[i], callStack[i-1])
+		}
+	}()
+
+	f() // Execute the root function
+}
+
+// Function to generate a flow tree as a string
+func generateFlowTree(root string) string {
+	callTreeMu.Lock()
+	defer callTreeMu.Unlock()
+
+	visited := make(map[string]bool)
+	var result string
+	var buildTree func(node string, depth int)
+	buildTree = func(node string, depth int) {
+		if visited[node] {
+			return
+		}
+		visited[node] = true
+		indentation := ""
+		for i := 0; i < depth; i++ {
+			indentation += "  "
+		}
+		result += fmt.Sprintf("%s- %s\n", indentation, node)
+		for _, child := range callTree[node] {
+			buildTree(child, depth+1)
+		}
+	}
+	buildTree(root, 0)
+	return result
 }
